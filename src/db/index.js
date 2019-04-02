524ce6b1c66840d8238846a50e2c38a432c57145
Yannick Scherer <yannick.scherer@futurice.com>
2019-04-02 14:02:45 +0200

    Move database specific logic into separate object

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 
+import MeetingModel from './models/meeting';
+
+// --- Promise Setup
 mongoose.Promise = global.Promise;
 
 // --- Connection to Database
 const connect = async ({ url }) => {
   console.debug('[db] connecting to database ...');
-  await mongoose.connect(url, { useNewUrlParser: true, })
+  await mongoose.connect(url, { useNewUrlParser: true });
   console.debug(`[db] successfully connected to '${url}'`);
 
   return {
@@ -13,13 +16,25 @@ const connect = async ({ url }) => {
   };
 };
 
-// --- Database Logic
+// --- Database Operations
+const Meeting = {
+  clear: () => MeetingModel.remove({}),
+  create: data => new MeetingModel(data).save(),
+  delete: async id => {
+    const meeting = await MeetingModel.findByIdAndRemove(id);
+    if (!meeting) {
+      throw new Error(`Meeting not found: ${id}`);
+    }
+  },
+  list: () => MeetingModel.find(),
+};
 
-// --- Database Constructor
+// --- Database Facade
 export default async config => {
   const database = await connect(config);
 
   return {
-    ...database
+    ...database,
+    Meeting,
   };
-}
+};
