cdadacc37831b284d60577e128199fea6d29165a
Yannick Scherer <yannick.scherer@futurice.com>
2019-05-10 12:29:07 +0200

    Add office-specific routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
+import { Router } from 'express';
+
 import meetingRoutes from './meetings';
 import healthRoutes from './health';
 
 export default (app, system) => {
-  meetingRoutes(app, system);
+  // --- General Routes
+  meetingRoutes(app, system); // Remove once no longer needed
   healthRoutes(app);
+
+  // --- Office Routes
+  const office = new Router();
+  meetingRoutes(office, system);
+  app.use('/:office', office);
 };
